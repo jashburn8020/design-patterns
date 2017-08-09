c869287120dcef19a4cb0802052b39adb314a0cb
jashburn8020 <jashburn8020@gmail.com>
2017-08-09 01:47:51 +0100

    Add JUnit dependency, and fix minor bug.

--- src/main/java/com/jashburn/designpatterns/behavioural/command/StorageReceiver.java
+++ src/main/java/com/jashburn/designpatterns/behavioural/command/StorageReceiver.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 class StorageReceiver implements Receiver {
 
-	Random random;
+	private Random random;
 
 	StorageReceiver() {
 		random = new Random(System.currentTimeMillis());
